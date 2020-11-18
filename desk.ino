e90d7d513f7bfdd43540e449c0ab74b65f037dab
spenshark <xzx01299@naver.com>
2020-11-18 22:31:40 +0900

    analog 수정

--- desk.ino
+++ desk.ino
@@ -12,12 +12,12 @@ long distance = 0;
 //setup, looop
 void setup()
 {
-    pinMode(A0, INPUT);
+    //
 }
 
 void loop()
 {
-    if(digitalRead(A0) == HIGH){
+    if(analogRead(A0) > 200){
         // 책상높이를 조절하는 함수 실행
     } else {
         // 실행 X
