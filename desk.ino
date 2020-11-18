be19ab331c6ec993b0cab7211aec1c4d86ae1f28
spenshark <xzx01299@naver.com>
2020-11-18 22:21:04 +0900

    초음파가 사라져서 하던게 날라갔기에,,, 이거 잘 넘어가면 회로나 구성할게요

--- desk.ino
+++ desk.ino
@@ -12,12 +12,17 @@ long distance = 0;
 //setup, looop
 void setup()
 {
-    //
+    pinMode(A0, INPUT);
 }
 
 void loop()
 {
-    //
+    if(digitalRead(A0) == HIGH){
+        // 책상높이를 조절하는 함수 실행
+    } else {
+        // 실행 X
+    }
+    delay(10);
 }
 
 //function definitions
