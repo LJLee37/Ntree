15fad9922a324430ca5e6cb14462df3d109b4b3e
SongInseo <andra123438@gmail.com>
2020-11-18 22:34:12 +0900

    Update desk.ino
    lcd 시간 표시

--- desk.ino
+++ desk.ino
@@ -1,23 +1,32 @@
 //headers
 #include <Serial.h>
-
+#include <LiquidCrystal.h>
 
 //global variables
 long time_sec = 0;
 long distance = 0;
 
-//function prototypes
+LiquidCrystal lcd(13, 12, 8, 9, 10, 11); //LiquidCrystal lcd(RS, Enable, DB4, DB5, DB6, DB7);
 
+//function prototypes
+void lcdPrint(int time);
 
 //setup, looop
 void setup()
 {
-    //
+    lcd.begin(16, 2);
 }
 
 void loop()
 {
-    //
+    lcdPrint(time_sec);
 }
 
 //function definitions
+void lcdPrint(int time)
+{
+    lcd.setCursor(0,0);
+    lcd.print("Sitting Time");
+    lcd.setCursor(0,1);
+    lcd.print("%d", time);
+}
