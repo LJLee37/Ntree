69c9a30f9026a67f62344650f75746b5c840585c
dongjoon <odobuy1004@naver.com>
2020-11-18 21:52:44 +0900

    first servo

--- desk.ino
+++ desk.ino
@@ -1,6 +1,10 @@
 //headers
 #include <Serial.h>
-
+#include <Servo.h>
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
 
 //global variables
 long time_sec = 0;
@@ -12,12 +16,20 @@ long distance = 0;
 //setup, looop
 void setup()
 {
-    //
+    servo1.attach(2);
+    servo2.attach(3);
+    servo3.attach(4);
+    servo4.attach(5);
+    Serial.begin(9600);
+    
 }
 
 void loop()
 {
-    //
+    
+    
+
 }
 
 //function definitions
+
