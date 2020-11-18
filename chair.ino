e6908ae61c839fd0955df32679a3b63c443e3737
LJLee37 <ljlee3759@gmail.com>
2020-11-18 22:54:13 +0900

    serial 통신 완료

--- chair.ino
+++ chair.ino
@@ -1,18 +1,22 @@
 //headers
 #include <Serial.h>
+#include <SoftwareSerial.h>
 
 
 //global variables
 long time_sec = 0;
+const int RX = 6, TX = 7;
+SoftwareSerial desk(RX, TX);
 
 
 //function prototypes
+int sendTime();
 
 
 //setup, looop
 void setup()
 {
-    //
+    desk.begin(9600);
 }
 
 void loop()
@@ -21,3 +25,7 @@ void loop()
 }
 
 //function definitions
+int sendTime()
+{
+    desk.println(time_sec);
+}
--- desk.ino
+++ desk.ino
@@ -1,18 +1,23 @@
 //headers
 #include <Serial.h>
+#include <SoftwareSerial.h>
 
 
 //global variables
 long time_sec = 0;
 long distance = 0;
+const int RX = 6, TX = 7;
+SoftwareSerial chair(RX, TX);
+
 
 //function prototypes
+int recieveTime();
 
 
 //setup, looop
 void setup()
 {
-    //
+    chair.begin(9600);
 }
 
 void loop()
@@ -21,3 +26,14 @@ void loop()
 }
 
 //function definitions
+int recieveTime()
+{
+    if (chair.available())
+    {
+        String recieved = chair.readStringUntil('\n');
+        time_sec = recieved.toInt();
+        return 0;
+    }
+    else
+        return 1;
+}
